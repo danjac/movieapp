b811220234cfd33f2289cc538f48ac756d3b38eb
Dan Jacob <danjac2018@gmail.com>
2020-12-12 19:44:30 +0200

    Lru cache

--- movieapp/common/tmdb_api.py
+++ movieapp/common/tmdb_api.py
@@ -10,10 +10,12 @@ import requests
 BASE_URL = "https://api.themoviedb.org/3/"
 
 
+@lru_cache
 def popular_movies():
     return _fetch_json("movie/popular")["results"]
 
 
+@lru_cache
 def now_playing():
     return _fetch_json("movie/now_playing")["results"]
 
@@ -22,6 +24,7 @@ def search_movies(search):
     return _fetch_json(f"search/movie?query={search}")["results"]
 
 
+@lru_cache
 def movie_genres():
     return _fetch_json("genre/movie/list")["genres"]
 
@@ -30,22 +33,26 @@ def movie(movie_id):
     return _fetch_json(f"movie/{movie_id}?append_to_response=credits,videos,images")
 
 
+@lru_cache
 def popular_tv():
     return _fetch_json("tv/popular")["results"]
 
 
+@lru_cache
 def top_rated_tv():
     return _fetch_json("tv/top_rated")["results"]
+
+
+@lru_cache
+def tv_genres():
+    return _fetch_json("genre/tv/list")["genres"]
 
 
 def tv_show(show_id):
     return _fetch_json(f"tv/{show_id}?append_to_response=credits,videos,images")
 
 
-def tv_genres():
-    return _fetch_json("genre/tv/list")["genres"]
-
-
+@lru_cache
 def popular_people(page=1):
     return _fetch_json(f"person/popular?page={page}")["results"]
 
@@ -62,7 +69,6 @@ def credits(person_id):
     return _fetch_json(f"person/{person_id}/credits")
 
 
-@lru_cache
 def _fetch_json(url):
     return requests.get(
         BASE_URL + url, headers={"Authorization": f"Bearer {settings.TMDB_API_TOKEN}"},
