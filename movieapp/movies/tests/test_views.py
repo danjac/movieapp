a17e309a2805bcad2a3f93f4047618689f13a9c0
Dan Jacob <danjac2018@gmail.com>
2020-12-12 15:35:30 +0200

    Tests

--- movieapp/movies/tests/test_views.py
+++ movieapp/movies/tests/test_views.py
@@ -7,17 +7,31 @@ from movieapp.common import tmdb_api
 # Local
 from .. import views
 
+movie = {
+    "id": 1,
+    "genre_ids": [1, 2],
+    "title": "Star Wars: A New Hope",
+    "poster_path": "/star-wars-a-new-hope.jpg",
+    "release_date": "1977-5-4",
+    "vote_average": 90,
+}
+
+genres = [
+    {"id": 1, "name": "Drama"},
+    {"id": 2, "name": "Sci-fi"},
+]
+
 
 class TestMovieList:
     def test_get(self, rf, mocker):
         mock_popular_movies = mocker.patch.object(tmdb_api, "popular_movies")
-        mock_popular_movies.return_value = []
+        mock_popular_movies.return_value = [movie]
 
         mock_now_playing = mocker.patch.object(tmdb_api, "now_playing")
-        mock_now_playing.return_value = []
-
+        mock_now_playing.return_value = [movie]
         mock_genres = mocker.patch.object(tmdb_api, "movie_genres")
-        mock_genres.return_value = []
+
+        mock_genres.return_value = genres
 
         resp = views.movie_list(rf.get(reverse("movies:movie_list")))
         assert resp.status_code == 200
